fcd4bc8320d46467c5a239e42cae57333bb3d60d
rodasgam <33024108+rodasgam@users.noreply.github.com>
2017-12-12 14:43:20 +0000

    g
    g

--- js/jquery.js
+++ js/jquery.js
@@ -1,8 +1,23 @@
-    $(document).ready(function() {
+$(document).ready(function() {
 
+    var a=new array();
 
-         $(#jogo).click(function () {
+    $("#btnadicionar").click(function(){
+
 
     });
 
+    var geografia_fácil= "https://opentdb.com/api.php?amount=10&category=22&difficulty=easy&type=multiple";
+    $.getJSON(geografia_fácil, function (data) {
+        console.log(data);
+        a = data;
+        localStorage.geografia_facil=data;
+
+    });
+
+    function () {
+
+    }
 });
+
+
