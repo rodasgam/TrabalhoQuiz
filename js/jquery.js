d60eb9db2ad2ed66e4f1c2bfdda91ab915fa2430
rodasgam <33024108+rodasgam@users.noreply.github.com>
2017-12-19 11:30:39 +0000

    egre
    eger

--- js/jquery.js
+++ js/jquery.js
@@ -1,23 +1,24 @@
 $(document).ready(function() {
 
-    var a=new array();
-
-    $("#btnadicionar").click(function(){
+    var a= new Array();
 
 
-    });
 
     var geografia_fácil= "https://opentdb.com/api.php?amount=10&category=22&difficulty=easy&type=multiple";
     $.getJSON(geografia_fácil, function (data) {
-        console.log(data);
-        a = data;
+        a = data.results;
         localStorage.geografia_facil=data;
 
     });
 
-    function () {
+    $("#btnAdicionar").on("click",function(){
+        localStorage.nome = $("#nome").val();
+        localStorage.dificuldade = $("#dificuldade").val();
+        localStorage.categoria = $("#categoria").val();
+        window.location.href = "jogo.html";
+        $("#cont").html("<h1>" + a[0].question + "</h1>");
+    });
 
-    }
 });
 
 
