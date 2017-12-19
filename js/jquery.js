3645abd67895a6998adce8850287e5e6a30e7925
rodasgam <33024108+rodasgam@users.noreply.github.com>
2017-12-19 11:43:48 +0000

    eee
    eee

--- js/jquery.js
+++ js/jquery.js
@@ -4,18 +4,18 @@ $(document).ready(function() {
 
 
 
-    var geografia_fácil= "https://opentdb.com/api.php?amount=10&category=22&difficulty=easy&type=multiple";
-    $.getJSON(geografia_fácil, function (data) {
+    var geografia_facil= "https://opentdb.com/api.php?amount=10&category=22&difficulty=easy&type=multiple";
+    $.getJSON(geografia_facil, function (data) {
         a = data.results;
-        localStorage.geografia_facil=data;
+        localStorage.set geografia_facil=data;
 
     });
 
     $("#btnAdicionar").on("click",function(){
-        localStorage.nome = $("#nome").val();
-        localStorage.dificuldade = $("#dificuldade").val();
-        localStorage.categoria = $("#categoria").val();
-        window.location.href = "jogo.html";
+        //localStorage.nome = $("#nome").val();
+        //localStorage.dificuldade = $("#dificuldade").val();
+        //localStorage.categoria = $("#categoria").val();
+        //window.location.href = "jogo.html";
         $("#cont").html("<h1>" + a[0].question + "</h1>");
     });
 
